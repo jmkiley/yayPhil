360e3fc47abb8e3648d3f4b68dd92e19c60f6b7a
Jordan Kiley <jordankiley@Jordans-MacBook-Air.local>
2016-10-06 16:10:07 -0400

    Added image, centered fed text

--- yayPhil/ViewController.swift
+++ yayPhil/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var philPicture: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 //        self.apiCall()
         createTextLabel()
+//        thePhilsImage()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,19 +24,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func thePhilsImage() {
+        philPicture.image = UIImage(contentsOfFile: "PhilKessel.JPG")
+        view.addSubview(philPicture)
+    }
+    
     func createTextLabel() {
         
         let date = getDate()
-        let label = UITextView(frame: CGRect(x: 40, y: 40, width: (self.view.frame.width - 40), height: (view.frame.height * 0.25)))
+        let label = UITextView(frame: CGRect(x: 40, y: 40, width: (self.view.frame.width - 80), height: (view.frame.height * 0.25)))
         view.addSubview(label)
         label.text = "Today is\n\(date) and \nPhil Kessel \nis a\nPittsburgh Penguin"
         label.textAlignment = .center
         label.textColor = UIColor(red:1.00, green:0.84, blue:0.00, alpha:1.0)
-//        label.font?.pointSize = 30
-        label.font = UIFont.systemFont(ofSize: 20.0)
-        label.backgroundColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0, alpha: 0)
-        
+
+        label.font = UIFont.boldSystemFont(ofSize: 20.0)
+        label.backgroundColor = UIColor(colorLiteralRed: 0, green: 0, blue: 1, alpha: 0)
+//        label.font = UIFont.
     }
+    
     
     func getDate() -> String {
 //        let dateAsDate = Date()
@@ -45,30 +53,6 @@ class ViewController: UIViewController {
 
         return dateAsString
     }
-    
-//    func apiCall() {
-//        
-//        let config = URLSessionConfiguration.default // Configure the session
-//        let session = URLSession(configuration: config)
-//        let url = URL(string: "https://api.twitter.com/1.1/statuses/user_timeline.json?count=5&user_id=389757150&screen_name=PKessel81&gvHcrP0XO3Plh5fpBX7Bhul7L=50dIPwmOjmJQc4qItFWOzAeVdG5Zc5JvLsnTG1fOErGMBzc07S")
-//        
-//        let task = session.dataTask(with: url!) { (data, response, error) in
-//            if error != nil {
-//                print(error?.localizedDescription)
-//            } else {
-//                do {
-//                    if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any] {
-//                        print(json)
-//                        print("Yay Phil!")
-//                    }
-//                } catch {
-//                    print(error)
-//                }
-//            }
-//        }
-//        task.resume()
-//        
-//    }
 
 }
 
